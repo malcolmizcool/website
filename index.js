fac47109f32bd819eca689692ccf66f5875b5de0
malcolm <malco@the-undertow-2.local>
2026-01-31 10:53:59 +1100

    guessing game

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+
+// clicker code. 
+
+
 let numberCount;
 let result;
 
@@ -20,3 +24,40 @@ document.getElementById("dec").onclick = function(){
 document.getElementById("reset").onclick = function(){
     document.getElementById("counter").textContent = 0
 }
+
+
+// guessing game code
+
+let submitButton = document.getElementById("submitButton");
+let input = document.getElementById("guessingGameInput");
+let resultGame = document.getElementById("result");
+let running = false;
+let guess;
+let guessingNumber;
+
+
+submitButton.onclick = function(){
+    if(!running){
+        guessingNumber = Math.floor(Math.random() * 100);
+    }
+    running = true
+    guess = Number(input.value)
+    if(isNaN(guess) || guess < 0 || guess > 100){
+        resultGame.textContent = "That is not a valid number"
+    }
+    else{
+        if(guess === guessingNumber){
+            resultGame.textContent = `That is correct! The number was ${guessingNumber}!`
+            running = false
+        }
+        else if(guess < guessingNumber){
+            resultGame.textContent = "Too low!"
+        }
+        else if(guess > guessingNumber){
+            resultGame.textContent = "Too high!"
+        }
+    }
+    
+    console.log(guessingNumber)
+}
+
