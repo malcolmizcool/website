ff3af9f4043e5d2ffc5d27d711fe7971c5069d50
malcolm <malco@the-undertow-2.local>
2026-01-30 16:04:40 +1100

    clicker update

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world!");
+
+let numberCount;
+let result;
+
+
+document.getElementById("inc").onclick = function(){
+    numberCount = document.getElementById("counter").textContent;
+    numberCount = Number(numberCount);
+    result = numberCount + 1
+    document.getElementById("counter").textContent = result
+}
+
+document.getElementById("dec").onclick = function(){
+    numberCount = document.getElementById("counter").textContent;
+    numberCount = Number(numberCount);
+    result = numberCount - 1
+    document.getElementById("counter").textContent = result
+}
+
+document.getElementById("reset").onclick = function(){
+    document.getElementById("counter").textContent = 0
+}
