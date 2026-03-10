9fc007de0e898cd5b1e2d45d6e4f61d9ab00e148
malcolm <malco@the-undertow-2.local>
2026-03-11 10:22:26 +1100

    migrate to flask

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/status')
+def status():
+    return render_template('status.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/empty')
+def empty():
+    return render_template('empty.html')
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
