2febe1bdc034167aac4742721ca1dbbffcba5533
malcolm <malco@the-undertow-2.local>
2026-03-11 14:41:55 +1100

    test

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def games():
     return render_template('games.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
